b34e0bca40915da9d647fe7eee46f69c9f3b7663
willfongqq <will.fong@qq.com>
2015-07-05 15:42:03 +0000

    First version

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# flaskr
+# flaskr 2.0
+
+flaskr 2.0 is a copy of the Flask Tutorial, but configured to use Redis
+instead of SQLite. This is so people can focus on learning Flask and not
+also having to learn about databases.
+
+Redis-Collections makes putting data from Python into a relatively permanent
+datastore.
+
+
+## Requirements
+
+pip install redis-collections
+pip install Flask
+
+
+## Start 
+
+sudo service redis-server start
+
+
+## Additional Reading
+
+- https://redis-collections.readthedocs.org/en/latest/
---
+++ flaskr.py
@@ -0,0 +1,57 @@
+from redis_collections import List
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+    render_template, flash
+     
+# configuration
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
+
+
+entries = List(key='flaskr')
+
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+
+@app.route('/')
+def show_entries():
+    return render_template('show_entries.html', entries=entries)
+
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    
+    entries.append(dict(title=request.form['title'], text=request.form['text']))
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
